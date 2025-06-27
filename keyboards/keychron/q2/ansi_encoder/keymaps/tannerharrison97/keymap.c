c50078b5b25ef1a7a09853de5a0ab30d59f2dda8
Tanner Harrison <tharrison@qualtrics.com>
2025-06-27 14:08:32 -0700

    update v to be a special on hold

--- keyboards/keychron/q2/ansi_encoder/keymaps/tannerharrison97/keymap.c
+++ keyboards/keychron/q2/ansi_encoder/keymaps/tannerharrison97/keymap.c
@@ -24,6 +24,10 @@ enum layers{
     _FN3
 };
 
+enum custom_keycodes {
+    V_PASTE = SAFE_RANGE,
+};
+
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
@@ -37,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TD(TD_GRV_TILD),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,           KC_HOME,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT, KC_UP,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    V_PASTE, KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT, KC_UP,
         KC_LCTL, KC_LOPT, KC_LCMD,                            KC_SPC,                             KC_RCMD, MO(_FN1), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_ansi_67(
         TD(TD_GRV_TILD),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,    KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,   KC_BSLS,          KC_DEL,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,             KC_ENT,           KC_HOME,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT, KC_UP,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    V_PASTE, KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,             KC_RSFT, KC_UP,
         KC_LCTL, KC_LWIN, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_FN2), MO(_FN3),  KC_LEFT, KC_DOWN, KC_RGHT),
 
     [_FN1] = LAYOUT_ansi_67(
@@ -73,6 +77,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 tap_dance_action_t tap_dance_actions[] = {
     [TD_GRV_TILD] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_TILD)
 };
+
+// Custom tap-hold for V key
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static uint16_t v_paste_timer;
+    
+    switch (keycode) {
+        case V_PASTE:
+            if (record->event.pressed) {
+                v_paste_timer = timer_read();
+            } else {
+                if (timer_elapsed(v_paste_timer) < TAPPING_TERM) {
+                    // Tap: send 'v'
+                    tap_code(KC_V);
+                } else {
+                    // Hold: send Cmd+Shift+V
+                    register_code(KC_LCMD);
+                    register_code(KC_LCTL);
+                    register_code(KC_LSFT);
+                    tap_code(KC_V);
+                    unregister_code(KC_LSFT);
+                    unregister_code(KC_LCMD);
+                    unregister_code(KC_LCTL);
+                }
+            }
+            return false;
+    }
+    return true;
+}
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
