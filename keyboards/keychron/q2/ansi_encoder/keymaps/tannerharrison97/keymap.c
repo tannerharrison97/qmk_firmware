f0b43f9f1769c2b7dd8a17da1a34e3063a9757a8
Tanner Harrison <tharrison@qualtrics.com>
2025-06-27 14:26:09 -0700

    playing with tap dancing

--- keyboards/keychron/q2/ansi_encoder/keymaps/tannerharrison97/keymap.c
+++ keyboards/keychron/q2/ansi_encoder/keymaps/tannerharrison97/keymap.c
@@ -36,6 +36,10 @@ enum {
     TD_GRV_ESC,
 };
 
+// Tap Dance function declarations
+void grv_esc_finished(tap_dance_state_t *state, void *user_data);
+void grv_esc_reset(tap_dance_state_t *state, void *user_data);
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_67(
         TD(TD_GRV_ESC),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
@@ -73,9 +77,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                            _______, _______,  _______,   _______, _______, _______)
 };
 
-// Simple tap dance: single tap = `, double tap = ~
+// Tap dance: single tap = `, double tap = ESC, 3+ taps = that many backticks
+void grv_esc_finished(tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:
+            register_code16(KC_GRV);
+            break;
+        case 2:
+            register_code16(KC_ESC);
+            break;
+        default:
+            // Send as many backticks as tap count (3, 4, 5, etc.)
+            for (int i = 0; i < state->count; i++) {
+                tap_code16(KC_GRV);
+            }
+            break;
+    }
+}
+
+void grv_esc_reset(tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:
+            unregister_code16(KC_GRV);
+            break;
+        case 2:
+            unregister_code16(KC_ESC);
+            break;
+        // Multiple taps don't need reset since we used tap_code16
+    }
+}
+
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_GRV_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_ESC)
+    [TD_GRV_ESC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, grv_esc_finished, grv_esc_reset)
 };
 
 // Custom tap-hold for V key
